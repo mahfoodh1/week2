9ff30e79bdb14f328556885bcb3dadf0640fdbd1
mahfoodh1 <mahfoodh@sheridancollege.ca>
2019-05-24 12:19:26 -0400

    add get and set method

---
+++ src/ca/sheridancollege/week1/softwarefudamentals/PartTimeStudent.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week1.softwarefudamentals;
+
+/**
+ *
+ * @author layth
+ */
+public class PartTimeStudent extends Students {
+    private String phone ;
+    
+    
+    public void setPhone( phone ) {
+        this.phone =phone;
+        
+         public void setPhone(  ) {
+         return this.phone ;
+        
+    }
+    
+}
